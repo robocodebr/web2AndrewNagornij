d05c59a11c81444659573575f041f8d561d2f39a
LAPTOP-HCRLML1C\Андрей <3102682al@gmail.com>
2020-11-01 18:11:13 +0200

    lesson 8

---
+++ lesson 8/main.js
@@ -0,0 +1,13 @@
+/*console.log("Hello!");
+let i = 0;
+while (i<10){
+    console.log(i);
+    i++;
+}
+while (i<20){
+    console.log(i);
+    i++;
+}*/
+for(let j = 0;j<10;j++){
+    console.log(j);
+}
